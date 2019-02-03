e6cf2b0ef9ac7233be27237337d0ae04c2c3a2ed
Will McAuliff <womcauliff@gmail.com>
2019-02-03 14:32:14 -0500

    Updates

---
+++ precache-manifest.7c5b08594847fd9ac74180a76ec7d132.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d21214da6131876faa340937f4acd544",
+    "url": "/PokeBattler-React/static/media/intro4.d21214da.gif"
+  },
+  {
+    "revision": "6278c9f9f680eb73d5c79b9f84f790f7",
+    "url": "/PokeBattler-React/static/media/intro3.6278c9f9.gif"
+  },
+  {
+    "revision": "b5f5d3bea1a43062f8da3fff57f380b4",
+    "url": "/PokeBattler-React/static/media/intro2.b5f5d3be.gif"
+  },
+  {
+    "revision": "278afa60067870b7da6f534427082da1",
+    "url": "/PokeBattler-React/static/media/intro1.278afa60.gif"
+  },
+  {
+    "revision": "957e5f1fe004a8d4fdef87e376d6ceb9",
+    "url": "/PokeBattler-React/static/media/101-opening.957e5f1f.mp3"
+  },
+  {
+    "revision": "cca6f45f5db73d090221",
+    "url": "/PokeBattler-React/static/js/runtime~main.cca6f45f.js"
+  },
+  {
+    "revision": "6dbc63043e08f0d5e2ab",
+    "url": "/PokeBattler-React/static/js/main.6dbc6304.chunk.js"
+  },
+  {
+    "revision": "3fe2504b4f99753d1d25",
+    "url": "/PokeBattler-React/static/js/1.3fe2504b.chunk.js"
+  },
+  {
+    "revision": "6dbc63043e08f0d5e2ab",
+    "url": "/PokeBattler-React/static/css/main.aef4ea53.chunk.css"
+  },
+  {
+    "revision": "03a2fd44ab738e74df968806f3b3babe",
+    "url": "/PokeBattler-React/index.html"
+  }
+];
